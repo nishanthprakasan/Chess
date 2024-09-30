96ef6e53a3d5136d9d9acc5dd827547ba0c7b54d
Nishanth Prakasan <84633000+nishanthprakasan@users.noreply.github.com>
2024-09-30 17:32:41 +0530

    Add files via upload

---
+++ loginscreen/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import register, user_login,play,user_logout, update_action , checkOpp
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('play/', play, name='play'),
+    path('logout/',user_logout,name='logout'),
+    path('update_action/', update_action, name='update_action'),
+    path('checkOpp/', checkOpp, name='checkOpp'),
+]
